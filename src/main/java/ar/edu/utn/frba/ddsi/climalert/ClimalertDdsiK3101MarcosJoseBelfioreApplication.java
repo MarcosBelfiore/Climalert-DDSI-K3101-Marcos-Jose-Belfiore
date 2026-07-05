bde11565eafa038d07839c6b3c5567adf817fa17
MarcosBelfiore <mbelfiore@frba.utn.edu.ar>
2026-07-05 20:01:39 -0300

    Creo el proyecto

---
+++ src/main/java/ar/edu/utn/frba/ddsi/climalert/ClimalertDdsiK3101MarcosJoseBelfioreApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertDdsiK3101MarcosJoseBelfioreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClimalertDdsiK3101MarcosJoseBelfioreApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ar/edu/utn/frba/ddsi/climalert/ClimalertDdsiK3101MarcosJoseBelfioreApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertDdsiK3101MarcosJoseBelfioreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
